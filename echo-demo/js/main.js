6075914c656215c5a1a2413d78f40458eba592c9
henrika <henrika@chromium.org>
2025-01-13 14:31:44 +0100

    More details

--- echo-demo/js/main.js
+++ echo-demo/js/main.js
@@ -2,13 +2,16 @@
 
 const gumAudio = document.getElementById('gum-audio');
 const gumButton = document.getElementById('gum');
+const gumAecCheckbox = document.getElementById('gum-aec');
 const gumStopButton = document.getElementById('gum-stop');
-const constraintsDiv = document.getElementById('constraints');
+const gumMuteCheckbox = document.getElementById('gum-mute');
+const gumConstraintsDiv = document.getElementById('gum-constraints');
 const errorElement = document.getElementById('error-message');
 
 let gumStream;
 
 gumStopButton.disabled = true;
+gumMuteCheckbox.disabled = true;
 
 const logi = (...args) => {
   console.log(...args);
@@ -20,15 +23,28 @@ const logw = (...args) => {
 
 const prettyJson = (obj) => JSON.stringify(obj, null, 2);
 
-const constraints = {
-      audio: true,
-      echoCancellation: {exact: true},
-      video: false,
-    };
-
 const loge = (error) => {
   errorElement.textContent = `DOMException: ${error.name} [${error.message}]`;
   console.error(error);
+};
+
+const printAudioSettings = (settings) => {
+  const propertiesToPrint = [
+      "autoGainControl",
+      "echoCancellation",
+      "noiseSuppression",
+      "sampleRate",
+      "voiceIsolation"
+    ];
+
+    //  MediaStreamTrack: getSettings is the current configuration of the track's constraints.
+    const filteredSettings = propertiesToPrint.reduce((obj, prop) => {
+      obj[prop] = settings[prop];
+      return obj;
+    }, {});
+    gumConstraintsDiv.textContent = 'Active constraints:\n' + prettyJson(filteredSettings);
+    // logi('capabilities:', prettyJson(audioTrack.getCapabilities()));
+    
 };
 
 gumButton.onclick = async () => {
@@ -36,16 +52,33 @@ gumButton.onclick = async () => {
     // const supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
     // logi(prettyJson(supportedConstraints));
     
+    const constraints = {
+      audio: true,
+      echoCancellation: {exact: gumAecCheckbox.checked},
+      video: false,
+    };
+    
+    logi('requested constraints to getUserMedia: ', constraints);
     gumStream = await navigator.mediaDevices.getUserMedia(constraints);
     const [audioTrack] = gumStream.getAudioTracks();
     logi('audioTrack:', audioTrack);
-    constraintsDiv.textContent = 'constraints:\n' + prettyJson(audioTrack.getSettings());
-    // logi('capabilities:', prettyJson(audioTrack.getCapabilities()));
+    
+    printAudioSettings(audioTrack.getSettings());
+     
+    audioTrack.onmute = () => {
+      logi('MediaStreamTrack.onmute: ' + audioTrack.label);
+      logi(audioTrack);
+    };
+    audioTrack.onunmute = () => {
+      logi('MediaStreamTrack.onmute: ' + audioTrack.label);
+      logi(audioTrack);
+    };
     
     gumAudio.srcObject = gumStream;
        
     gumButton.disabled = true;
     gumStopButton.disabled = false;
+    gumMuteCheckbox.disabled = false;
   } catch (e) {
     loge(e);
   }
@@ -58,6 +91,14 @@ gumStopButton.onclick = () => {
     gumAudio.srcObject = null;
     gumButton.disabled = false;
     gumStopButton.disabled = true;
+    gumMuteCheckbox.disabled = true;
+  }
+};
+
+gumMuteCheckbox.onchange = () => {
+  if (gumStream) {
+    const [track] = gumStream.getAudioTracks();
+    track.enabled = !gumMuteCheckbox.checked;
   }
 };
 
