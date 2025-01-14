69cd0a1b09254dc95499aac170495b04a0ae9024
henrika <henrika@chromium.org>
2025-01-14 17:37:02 +0100

    Added MediaStreamRecorder

--- echo-demo/js/main.js
+++ echo-demo/js/main.js
@@ -1,19 +1,26 @@
 'use strict';
 
 const gumAudio = document.getElementById('gum-audio');
+const gumRecordedAudio = document.getElementById('gum-recorded-audio');
 const gumButton = document.getElementById('gum');
+const gumRecordButton = document.getElementById('gum-record');
 const gumAecCheckbox = document.getElementById('gum-aec');
 const gumStopButton = document.getElementById('gum-stop');
 const gumMuteCheckbox = document.getElementById('gum-mute');
 const gumConstraintsDiv = document.getElementById('gum-constraints');
+const gumTrackDiv = document.getElementById('gum-track');
 const errorElement = document.getElementById('error-message');
 
 let htmlAudio;
 let gumStream;
+let mediaRecorder;
+let recordedBlobs;
 
 gumStopButton.disabled = true;
 gumMuteCheckbox.disabled = true;
 gumAecCheckbox.disabled = false;
+
+const mimeType = 'audio/mp4';
 
 const logi = (...args) => {
   console.log(...args);
@@ -32,8 +39,199 @@ const loge = (error) => {
 
 document.addEventListener('DOMContentLoaded', (event) => {
   htmlAudio = document.getElementById("html-audio");
-  htmlAudio.volume = 0.5; 
+  htmlAudio.volume = 0.3; 
 });
+
+function clearGumInfoContainer() {
+  const container = document.querySelector('.gum-info-container');
+  const divsToClear = container.querySelectorAll('div');
+  divsToClear.forEach(div => {
+    div.textContent = '';
+  });
+};
+
+function printAudioSettings(settings) {
+  const propertiesToPrint = [
+    'echoCancellation',
+    'autoGainControl',
+    'noiseSuppression',
+    'sampleRate',
+    'voiceIsolation'
+  ];
+
+  //  MediaStreamTrack: getSettings is the current configuration of the track's constraints.
+  const filteredSettings = propertiesToPrint.reduce((obj, prop) => {
+    obj[prop] = settings[prop];
+    return obj;
+  }, {});
+  gumConstraintsDiv.textContent = 'Active constraints:\n' + prettyJson(filteredSettings);
+  // logi('capabilities:', prettyJson(audioTrack.getCapabilities()));
+    
+};
+
+function printAudioTrack(track) {
+  const propertiesToPrint = [
+    'label',
+    'kind',
+    'enabled',
+    'muted',
+    'readyState'
+  ];
+  const filteredTrack = propertiesToPrint.reduce((obj, prop) => {
+    obj[prop] = track[prop];
+    return obj;
+  }, {});
+  gumTrackDiv.textContent = 'MediaStreamTrack:\n' + prettyJson(filteredTrack);
+};
+
+gumButton.onclick = async () => {
+  try {
+    // const supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
+    // logi(prettyJson(supportedConstraints));
+    
+    const constraints = {
+      audio: {
+        echoCancellation: {exact: gumAecCheckbox.checked},
+        autoGainControl: {exact: true},
+        noiseSuppression: {exact: true},
+      },
+      video: false,
+    };
+    
+    logi('requested constraints to getUserMedia: ', prettyJson(constraints));
+    gumStream = await navigator.mediaDevices.getUserMedia(constraints);
+    const [audioTrack] = gumStream.getAudioTracks();
+    logi('audioTrack:', audioTrack);
+    
+    printAudioSettings(audioTrack.getSettings());
+    printAudioTrack(audioTrack);
+     
+    audioTrack.onmute = () => {
+      logi('MediaStreamTrack.onmute: ' + audioTrack.label);
+      printAudioTrack(audioTrack);
+    };
+    audioTrack.onunmute = () => {
+      logi('MediaStreamTrack.onmute: ' + audioTrack.label);
+      printAudioTrack(audioTrack);
+    };
+    
+    gumAudio.srcObject = gumStream;
+       
+    gumButton.disabled = true;
+    gumStopButton.disabled = false;
+    gumMuteCheckbox.disabled = false;
+    gumAecCheckbox.disabled = true;
+    gumRecordButton.disabled = false;
+  } catch (e) {
+    loge(e);
+  }
+};
+
+gumStopButton.onclick = () => {
+  if (gumStream) {
+    const [track] = gumStream.getAudioTracks();
+    track.stop();
+    gumAudio.srcObject = null;
+    gumButton.disabled = false;
+    gumStopButton.disabled = true;
+    gumMuteCheckbox.disabled = true;
+    gumAecCheckbox.disabled = false;
+    gumRecordButton.textContent = 'Start Recording';
+    gumRecordButton.disabled = true;
+    gumRecordedAudio.src = '';
+    gumRecordedAudio.disabled = true;
+    clearGumInfoContainer();
+  }
+};
+
+gumMuteCheckbox.onchange = () => {
+  if (gumStream) {
+    const [track] = gumStream.getAudioTracks();
+    track.enabled = !gumMuteCheckbox.checked;
+    printAudioTrack(track);
+  }
+};
+
+function startRecording() {
+  if (!gumStream) {
+    return;
+  }
+  
+  gumRecordedAudio.src = '';
+  gumRecordedAudio.disabled = true;
+  
+  recordedBlobs = [];
+  const options = {mimeType};
+  if (!MediaRecorder.isTypeSupported(mimeType)) {
+    console.error(`MediaRecorder does not support mimeType: ${mimeType}`);
+    return;
+  }
+  
+  try {
+    mediaRecorder = new MediaRecorder(gumStream, options);
+    gumRecordButton.textContent = 'Stop Recording';
+    
+    mediaRecorder.onstop = (event) => {
+      
+      gumRecordedAudio.addEventListener('canplay', () => {
+        logi('Recorded audio is now ready to be played out and/or downloaded.');
+      });
+      
+      const superBuffer = new Blob(recordedBlobs, {type: mimeType});
+      gumRecordedAudio.src = '';
+      gumRecordedAudio.srcObject = null;
+      gumRecordedAudio.src = URL.createObjectURL(superBuffer);
+    };
+    
+    mediaRecorder.ondataavailable = (event) => {
+      if (event.data && event.data.size > 0) {
+        recordedBlobs.push(event.data);
+      }
+    };
+    
+    mediaRecorder.start();
+  } catch (e) {
+    log(e); 
+  }
+};
+
+function stopRecording() {
+  if (mediaRecorder) {
+    mediaRecorder.stop();
+    mediaRecorder = null;
+  }
+};
+
+gumRecordButton.onclick = () => {
+  if (gumRecordButton.textContent === 'Start Recording') {
+    startRecording();
+  } else {
+    stopRecording();
+    gumRecordButton.textContent = 'Start Recording';
+  }
+};
+
+// gumButton.innerHTML = "&#x25B6;&#xFE0F;" : "&#x23F8;&#xFE0F;";
+
+/*
+Calling MediaStreamTrack: applyConstraints with `echoCancellation` as constraint
+results in: DOMException: OverconstrainedError [Cannot satisfy constraints].
+gumAecCheckbox.onchange = async () => {
+  if (gumStream) {
+    try {
+      const [track] = gumStream.getAudioTracks();
+      const constraints = {
+        echoCancellation: {exact: gumAecCheckbox.checked},
+      }
+      logi('requested constraints to applyConstraints: ', prettyJson(constraints));
+      await track.applyConstraints(constraints);
+      printAudioSettings(track.getSettings());
+    } catch (e) {
+      loge(e);
+    }
+  }
+};
+*/
 
 /*
 htmlAudio.addEventListener('play', (event) => {
@@ -73,104 +271,5 @@ htmlAudio.addEventListener('error', (event) => {
 
   console.error(errorMessage);
 });
-*/
-
-const printAudioSettings = (settings) => {
-  const propertiesToPrint = [
-      "echoCancellation",
-      "autoGainControl",
-      "noiseSuppression",
-      "sampleRate",
-      "voiceIsolation"
-    ];
-
-    //  MediaStreamTrack: getSettings is the current configuration of the track's constraints.
-    const filteredSettings = propertiesToPrint.reduce((obj, prop) => {
-      obj[prop] = settings[prop];
-      return obj;
-    }, {});
-    gumConstraintsDiv.textContent = 'Active constraints:\n' + prettyJson(filteredSettings);
-    // logi('capabilities:', prettyJson(audioTrack.getCapabilities()));
-    
-};
-
-gumButton.onclick = async () => {
-  try {
-    // const supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
-    // logi(prettyJson(supportedConstraints));
-    
-    const constraints = {
-      audio: {
-        echoCancellation: {exact: gumAecCheckbox.checked},
-        autoGainControl: {exact: true},
-        noiseSuppression: {exact: true},
-      },
-      video: false,
-    };
-    
-    logi('requested constraints to getUserMedia: ', prettyJson(constraints));
-    gumStream = await navigator.mediaDevices.getUserMedia(constraints);
-    const [audioTrack] = gumStream.getAudioTracks();
-    logi('audioTrack:', audioTrack);
-    
-    printAudioSettings(audioTrack.getSettings());
-     
-    audioTrack.onmute = () => {
-      logi('MediaStreamTrack.onmute: ' + audioTrack.label);
-      logi(audioTrack);
-    };
-    audioTrack.onunmute = () => {
-      logi('MediaStreamTrack.onmute: ' + audioTrack.label);
-      logi(audioTrack);
-    };
-    
-    gumAudio.srcObject = gumStream;
-       
-    gumButton.disabled = true;
-    gumStopButton.disabled = false;
-    gumMuteCheckbox.disabled = false;
-    gumAecCheckbox.disabled = true;
-  } catch (e) {
-    loge(e);
-  }
-};
-
-gumStopButton.onclick = () => {
-  if (gumStream) {
-    const [track] = gumStream.getAudioTracks();
-    track.stop();
-    gumAudio.srcObject = null;
-    gumButton.disabled = false;
-    gumStopButton.disabled = true;
-    gumMuteCheckbox.disabled = true;
-    gumAecCheckbox.disabled = false;
-  }
-};
-
-gumMuteCheckbox.onchange = () => {
-  if (gumStream) {
-    const [track] = gumStream.getAudioTracks();
-    track.enabled = !gumMuteCheckbox.checked;
-  }
-};
-
-/*
-Calling MediaStreamTrack: applyConstraints with `echoCancellation` as constraint
-results in: DOMException: OverconstrainedError [Cannot satisfy constraints].
-gumAecCheckbox.onchange = async () => {
-  if (gumStream) {
-    try {
-      const [track] = gumStream.getAudioTracks();
-      const constraints = {
-        echoCancellation: {exact: gumAecCheckbox.checked},
-      }
-      logi('requested constraints to applyConstraints: ', prettyJson(constraints));
-      await track.applyConstraints(constraints);
-      printAudioSettings(track.getSettings());
-    } catch (e) {
-      loge(e);
-    }
-  }
-};
 */
 
